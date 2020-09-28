0b3f6ee9cb8ee7285b97b7e69b303e7c378b718c
gilsonsu <45291900+gilsonsu@users.noreply.github.com>
2020-09-27 21:53:40 -0300

    Create MainActivity.kt
    Praticando Flutter e Dart

---
+++ android/app/src/main/kotlin/gsu/a20200214/flutter_pdv/MainActivity.kt
@@ -0,0 +1,12 @@
+package gsu.a20200214.flutter_pdv
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
